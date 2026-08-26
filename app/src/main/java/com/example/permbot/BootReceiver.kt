7e50344acb365b50b0290c7eeac79708a79f64a0
textgram <alishaeow@gmail.com>
2026-08-27 03:54:12 +0530

    Create BootReceiver.kt

---
+++ app/src/main/java/com/example/permbot/BootReceiver.kt
@@ -0,0 +1,19 @@
+package com.example.permbot
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
+            val serviceIntent = Intent(context, BotService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
